0f9d5012fc04d9ce47300680bf85f9518b39dd95
wachidmuslih <wachidmslh@gmail.com>
2021-10-05 21:51:31 +0700

    Add project files.

---
+++ FirstConsoleApp_110/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstConsoleApp_110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
